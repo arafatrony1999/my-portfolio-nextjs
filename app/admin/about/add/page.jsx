9b60b269fce3df7731742524b3651b754037ed55
arafatrony1999 <arafat.rony1999@gmail.com>
2026-04-18 20:32:51 +0600

    admin add about page

---
+++ app/admin/about/add/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+const AddAboutPage = () => {
+  return (
+    <div>
+      <h1>Add About Page</h1>
+      {/* Add your form or content here */}
+    </div>
+  );
+};
+
+export default AddAboutPage;
